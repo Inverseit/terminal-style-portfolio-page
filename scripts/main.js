a4ddc74754887ee8975ee65e5b9615c114cdf2d1
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2021-12-24 19:09:04 +0600

    final

--- scripts/main.js
+++ scripts/main.js
@@ -1,155 +1,176 @@
-
 const app = document.querySelector("#app");
-const delay = ms => new Promise(res => setTimeout(res, ms));
-    
-    
-app.addEventListener("keypress", async function(event){
-  if(event.key === "Enter"){
+let signing = false;
+
+const commands = {
+  about: {
+    text: "Who are we and what are we doing?",
+    action: () => {
+      createText(
+        "We are some people and we really want a linux lab at CMUQ!"
+      );
+    },
+  },
+  help: {
+    text: "See all commands.",
+    action: () => {
+      for (const [key, { text }] of Object.entries(commands)) {
+        createCode(key, text);
+      }
+    },
+  },
+  sign: {
+    text: "Support our initiation - sign the petition by entering your email",
+    action: () => {
+      signing = true;
+      createText("Please, enter your email:");
+    },
+  },
+  stats: {
+    text: "See how many people have signed the petition",
+    action: async () => {
+      createText("Fetching data from the server:");
+      const res = await stats();
+      createText(`${res} people have signed the petition`)
+    },
+  },
+};
+
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+app.addEventListener("keypress", async function (event) {
+  if (event.key === "Enter") {
     await delay(150);
-   getInputValue();
-   
+    getInputValue();
+
     removeInput();
     await delay(150);
     new_line();
   }
 });
 
-app.addEventListener("click", function(event){
+app.addEventListener("click", function (event) {
   const input = document.querySelector("input");
   input.focus();
-})
+});
 
-
-async function open_terminal(){
+async function open_terminal() {
   createText("Welcome");
   await delay(700);
   createText("Starting the server...");
   await delay(1500);
   createText("You can run several commands:");
- 
-  createCode("about me", "Who am i and what do i do.");
-  createCode("all", "See all commands.");
-  createCode("social -a", "All my social networks.");
+
+  for (const [key, { text }] of Object.entries(commands)) {
+    createCode(key, text);
+  }
 
   await delay(500);
   new_line();
 }
 
-
-function new_line(){
-  
-  const p = document.createElement("p");
-  const span1 = document.createElement("span");
-  const span2 = document.createElement("span");
-  p.setAttribute("class", "path")
-  p.textContent = "# user";
-  span1.textContent = " in";
-  span2.textContent = " ~/heber-leonard";
-  p.appendChild(span1);
-  p.appendChild(span2);
-  app.appendChild(p);
+function new_line() {
+  if (!signing){
+    const p = document.createElement("p");
+    const span1 = document.createElement("span");
+    const span2 = document.createElement("span");
+    p.setAttribute("class", "path");
+    p.textContent = "# user";
+    span1.textContent = " in";
+    span2.textContent = " ~/linux_lab";
+    p.appendChild(span1);
+    p.appendChild(span2);
+    app.appendChild(p);
+  }
   const div = document.createElement("div");
-  div.setAttribute("class", "type")
+  div.setAttribute("class", "type");
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
+  i.setAttribute("class", "fas fa-angle-right icone");
   const input = document.createElement("input");
   div.appendChild(i);
   div.appendChild(input);
   app.appendChild(div);
   input.focus();
-  
 }
 
-function removeInput(){
+function removeInput() {
   const div = document.querySelector(".type");
   app.removeChild(div);
 }
 
-async function getInputValue(){
-  
+async function getInputValue() {
   const value = document.querySelector("input").value;
-  if(value === "all"){
+  if (signing){
+    createText(value);
+    const {isSuccess, message} = await signByEmail(value);
+    if (isSuccess){
+      trueValue(message);
+    } else{
+      falseValue(message);
+    }
+    signing = false;
+    return;
+  }
+  if (commands.hasOwnProperty(value)) {
     trueValue(value);
-    
-    createCode("projects", "My github page with my projects. Follow me there ;)");
-    createCode("about me", "Who am i and what do i do.");
-    createCode("social -a", "All my social networks.");
-    createCode("clear", "Clean the terminal.");
-    
-  }
-  else if(value === "projects"){
-    trueValue(value);
-    createText("<a href='https://github.com/heberleonard2' target='_blank'><i class='fab fa-github white'></i> github.com/heberleonard2</a>")
-  }
-  else if(value === "about me"){
-    trueValue(value);
-    createText("Oi, meu nome é Héber ;)")
-    createText("Desenvolvedor atualmente focado em todo o ecossistema Javascript. Utilizando principalmente a stack <span class='blue'>Node, React e React Native </span>por permitir criar aplicações de forma descomplicada e produtiva.")
-  }
-  else if(value === "social -a"){
-    trueValue(value);
-    createText("<a href='https://github.com/heberleonard2' target='_blank'><i class='fab fa-github white'></i> github.com/heberleonard2</a>")
-    createText("<a href='https://www.linkedin.com/in/heber-leonard/' target='_blank'><i class='fab fa-linkedin-in white'></i> linkedin.com/in/heber-leonard</a>")
-    createText("<a href='https://www.instagram.com/heber_leonard/' target='_blank'><i class='fab fa-instagram white'></i> instagram.com/heber_leonard</a>")
-  }
-  else if(value === "social"){
-    trueValue(value);
-    createText("Didn't you mean: social -a?")
-  }
-  
-  else if(value === "clear"){
-    document.querySelectorAll("p").forEach(e => e.parentNode.removeChild(e));
-    document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e));
-  }
-  else{
+    commands[value].action();
+  } else if (value === "clear") {
+    document.querySelectorAll("p").forEach((e) => e.parentNode.removeChild(e));
+    document
+      .querySelectorAll("section")
+      .forEach((e) => e.parentNode.removeChild(e));
+  } else {
     falseValue(value);
-    createText(`command not found: ${value}`)
+    createText(`command not found: ${value}`);
   }
 }
 
-function trueValue(value){
-  
+function trueValue(value) {
   const div = document.createElement("section");
-  div.setAttribute("class", "type2")
+  div.setAttribute("class", "type2");
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone")
+  i.setAttribute("class", "fas fa-angle-right icone");
   const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "sucess")
+  mensagem.setAttribute("class", "sucess");
   mensagem.textContent = `${value}`;
   div.appendChild(i);
   div.appendChild(mensagem);
   app.appendChild(div);
 }
 
-function falseValue(value){
-  
+function falseValue(value) {
   const div = document.createElement("section");
-  div.setAttribute("class", "type2")
+  div.setAttribute("class", "type2");
   const i = document.createElement("i");
-  i.setAttribute("class", "fas fa-angle-right icone error")
+  i.setAttribute("class", "fas fa-angle-right icone error");
   const mensagem = document.createElement("h2");
-  mensagem.setAttribute("class", "error")
+  mensagem.setAttribute("class", "error");
   mensagem.textContent = `${value}`;
   div.appendChild(i);
   div.appendChild(mensagem);
   app.appendChild(div);
 }
 
-function createText(text, classname){
+function createText(text, classname) {
   const p = document.createElement("p");
-  
-  p.innerHTML =
-  text
-  ;
+
+  p.innerHTML = text;
   app.appendChild(p);
 }
 
-function createCode(code, text){
+function createCode(code, text) {
   const p = document.createElement("p");
   p.setAttribute("class", "code");
-  p.innerHTML =
- `${code} <br/><span class='text'> ${text} </span>`;
+  p.innerHTML = `${code} <br/><span class='text'> ${text} </span>`;
   app.appendChild(p);
 }
 
 open_terminal();
+
+const signByEmail = async (email) => {
+  console.log(email);
+  return {isSuccess: true, message: `Done, thanks ${email}!`}
+}
+
+const stats = async () => {
+  return 10;
+}
